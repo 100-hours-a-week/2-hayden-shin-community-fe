b1efbec4aa229e24c8bbe03d8421d0a46d632be0
hayden <haydenhyo@gmail.com>
2025-01-27 11:41:34 +0900

    feat: update BASE_URL to use HTTPS with public EC2 DNS

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://3.35.218.197/api';
+export const BASE_URL = 'https://ec2-3-35-218-197.ap-northeast-2.compute.amazonaws.com/api';
 export const CDN_URL = 'https://d222sz9qo9ux2.cloudfront.net';
