fd353bbbacf50f7451d05cb18e38869545026e26
hayden <haydenhyo@gmail.com>
2025-01-27 13:54:40 +0900

    chore: update BASE_URL to EB deployment address
    - Set BASE_URL to 'http://hayden-server.ap-northeast-2.elasticbeanstalk.com/api'
    - Reflect new EB environment configuration for client-side requests

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://ec2-3-35-218-197.ap-northeast-2.compute.amazonaws.com/api';
+export const BASE_URL = 'http://hayden-server.ap-northeast-2.elasticbeanstalk.com/api';
 export const CDN_URL = 'https://d222sz9qo9ux2.cloudfront.net';
