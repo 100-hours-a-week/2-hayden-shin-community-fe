d35789f57f17f8256a03874ec16bf5122ea8a296
hayden <haydenhyo@gmail.com>
2025-02-02 21:37:48 +0900

    chore: setup production environment configuration

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://hayden-server.ap-northeast-2.elasticbeanstalk.com/api';
+export const BASE_URL = 'http://3.34.199.170/api';
 export const CDN_URL = 'https://d222sz9qo9ux2.cloudfront.net';
